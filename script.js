991f2c34386427d8ed3000923c94dd20257f91e1
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-12 20:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  const n = arr.length;
+  let closestSum = Infinity;
+
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        const sum = arr[i] + arr[j] + arr[k];
+        const diff = Math.abs(sum - target);
+
+        if (diff < Math.abs(closestSum - target)) {
+          closestSum = sum;
+        }
+      }
+    }
+  }
+
+  return closestSum;
 }
 
 module.exports = threeSum;
